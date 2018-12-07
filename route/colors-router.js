a2f138789a6178d1fb578aaff602d4d2935394a0
penssake <penssake@gmail.com>
2018-12-07 04:12:13 -0800

    random search functional

--- route/colors-router.js
+++ route/colors-router.js
@@ -5,6 +5,24 @@ const superagent = require('superagent')
 
 const colorsRouter = module.exports = new Router()
 
+const filterRandomResult = (data) => {
+  const result = []
+  for(let color of data) {
+    let redHex = color.red.toString(16)
+    let greenHex = color.green.toString(16)
+    let blueHex = color.blue.toString(16)
+    let rgb = redHex + greenHex + blueHex
+    result.push({
+      _id: color._id.$oid,
+      red: color.red,
+      green: color.green,
+      blue: color.blue,
+      label: rgb,
+    })
+  }
+  return result[Math.floor(Math.random() * result.length)]
+}
+
 const filteredFrontendData = (data) => {
   const result = []
   for(let color of data) {
@@ -25,6 +43,7 @@ const filteredFrontendData = (data) => {
 }
 
 const filterClicked = (data) => {
+  console.log('YOU WAT', data)
   const result = []
   let redHex = data.red.toString(16)
   let greenHex = data.green.toString(16)
@@ -41,9 +60,10 @@ const filterClicked = (data) => {
   return result
 }
 
-colorsRouter.get('/colorsAll', (request, response, next) => {
+colorsRouter.get('/colorsAll/:page', (request, response, next) => {
+  let PAGE = request.params.page
   return superagent
-    .get(process.env.API_URL)
+    .get(`${process.env.API_SKIP}${PAGE}${process.env.LIMIT}`)
     .then(data => {
       return response.json(filteredFrontendData(JSON.parse(data.text)))
     }).catch(err => console.error(err))
@@ -52,17 +72,26 @@ colorsRouter.get('/colorsAll', (request, response, next) => {
 colorsRouter.get('/colors/:clickedID', (request, response, next) => {
   let ID = request.params.clickedID
   return superagent
-    .get(`https://api.mlab.com/api/1/databases/color-palette/collections/colors/${ID}?apiKey=To-iC7DnLVcl359c9nyN52ac49Hcky7H`)
+    .get(`${process.env.API_ID}/${ID}?${process.env.API_KEY}`)
     .then(data => {
       return response.json(filterClicked(JSON.parse(data.text)))
     }).catch(err => console.error(err))
 })
 
-// colorsRouter.get('/colors/random', (request, response, next) => {
-//     let ID = request.params.clickedID
-//     return superagent
-//     .get(process.env.API_URL)
-//     .then(data => {
-//         return response.json(filterClicked(JSON.parse(data.text)));
-//     }).catch(err => console.error(err))
-// })
+colorsRouter.get('/random', (request, response, next) => {
+  return superagent
+    .get(`${process.env.API_URL}`)
+    .then(data => {
+      return response.json(filterRandomResult(JSON.parse(data.text)))
+    }).catch(err => console.error(err))
+})
+
+colorsRouter.get('/colorQuery/:color', (request, response, next) => {
+  console.log(request.params.color)
+  let selectedColor = request.params.color
+  return superagent
+    .get(`${process.env.COLOR_QUERY}{"primaryColor": "${selectedColor}"}&${process.env.API_KEY}`)
+    .then(data => {
+      return response.json(filteredFrontendData(JSON.parse(data.text)))
+    }).catch(err => console.error(err))
+})
